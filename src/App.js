5167a6a69631fe9303dff633cb027b1c227e864e
jpaetsch <j.paetsch@live.com>
2022-05-11 22:47:10 -0600

    Customized favicons and manifest contents including document title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Layout from './components/Layout'
 import About from './components/About';
