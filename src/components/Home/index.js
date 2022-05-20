3fe1a255573c38ee90f666e1d8c9093586c351be
jpaetsch <j.paetsch@live.com>
2022-05-19 22:59:19 -0600

    Added home page image es and content

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,10 @@ import Loader from 'react-loaders';
 import { Link } from 'react-router-dom';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
-import ProfilePic from '../../assets/images/profile-pic.jpg'
+import homeBackground1 from '../../assets/images/homeBackground1.jpg'
+import homeBackground2 from '../../assets/images/homeBackground2.jpg'
+import homeBackground3 from '../../assets/images/homeBackground3.jpg'
+import homeBackground4 from '../../assets/images/homeBackground4.jpg'
 
 
 const Home = () => {
@@ -34,57 +37,52 @@ const Home = () => {
                     <span>
                         Computer Engineer<br />
                         Software Developer<br />
-                        Data Analyst<br />
+                        Business Analyst<br />
                         Lifelong Learner
                     </span>
                 </div>
                 <Link to='/contact' className='contact-button'>CONTACT ME</Link>
             </div>
-            {/* <div className='image-slides-container'>
-                <div className='slide-content'>
-                    <p className='slide-content'>Slide #1 Content</p>
-                </div>
-            </div> */}
-
             <div className='timeline'>
-                <div className="tl-item">
-                    <div className="tl-bg im1"></div>
-                    <div className="tl-year">
-                        <p className="f2 heading--sanSerif">2011</p>
+                <div className='tl-item'>
+                    <div className='tl-bg' style={{backgroundImage: `url(${homeBackground4})`}}></div>
+                    <div className='tl-year'>
+                        <p>Work</p>
                     </div>
-                    <div className="tl-content">
-                        <h1>Lorem ipsum dolor sit</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
-                    </div>
-                </div>
-                <div className="tl-item">
-                    <div className="tl-bg im2"></div>
-                    <div className="tl-year">
-                        <p className="f2 heading--sanSerif">2013</p>
-                    </div>
-                    <div className="tl-content">
-                        <h1 className="f3 text--accent ttu">Vestibulum laoreet lorem</h1>
-                        <p>Suspendisse potenti. Sed sollicitudin eros lorem, eget accumsan risus dictum id. Maecenas dignissim ipsum vel mi rutrum egestas. Donec mauris nibh, facilisis ut hendrerit vel, fringilla sed felis. Morbi sed nisl et arcu.</p>
+                    <div className='tl-content'>
+                        <h1>My Skills & Experience</h1>
+                        <p>Throughout my degree and outside of school, I've worked at a number of companies and gained exposure to a wide range of technical skills.<br /><br />From backend system frameworks to frontend design libraries, I have experience in many different programming languages and architectural design concepts.</p>
+                        {/* <Link to='/contact' className='contact-button'>CONTACT ME</Link> */}
                     </div>
                 </div>
-                <div className="tl-item">
-                    <div className="tl-bg im3"></div>
-                    <div className="tl-year">
-                        <p className="f2 heading--sanSerif">2014</p>
+                <div className='tl-item'>
+                    <div className='tl-bg' style={{backgroundImage: `url(${homeBackground3})`}}></div>
+                    <div className='tl-year'>
+                        <p>Projects</p>
                     </div>
-                    <div className="tl-content">
-                        <h1 className="f3 text--accent ttu">Phasellus mauris elit</h1>
-                        <p>Mauris cursus magna at libero lobortis tempor. Suspendisse potenti. Aliquam interdum vulputate neque sit amet varius. Maecenas ac pulvinar nisi. Fusce vitae nunc ultrices, tristique dolor at, porttitor dolor. Etiam id cursus arcu, in dapibus nibh. Pellentesque non porta leo. Nulla eros odio, egestas quis efficitur vel, pretium sed.</p>
+                    <div className='tl-content'>
+                        <h1>My Showcases</h1>
+                        <p>There's no better way to learn than to get hands-on. I have a number of projects I like to develop outside of work to help with my own learning.<br /><br />One of these projects includes making this website from scratch using React and SCSS! See my GitHub repository for additional projects and code samples.</p>
                     </div>
                 </div>
-                <div className="tl-item">
-                    <div className="tl-bg im4"></div>
-                    <div className="tl-year">
-                        <p className="f2 heading--sanSerif">2016</p>
+                <div className='tl-item'>
+                    <div className='tl-bg' style={{backgroundImage: `url(${homeBackground2})`}}></div>
+                    <div className='tl-year'>
+                        <p>Hobbies</p>
                     </div>
-                    <div className="tl-content">
-                        <h1 className="f3 text--accent ttu">Mauris vitae nunc elem</h1>
-                        <p>Suspendisse ac mi at dolor sodales faucibus. Nunc sagittis ornare purus non euismod. Donec vestibulum efficitur tortor, eget efficitur enim facilisis consequat. Vivamus laoreet laoreet elit. Ut ut varius metus, bibendum imperdiet ex. Curabitur diam quam, blandit at risus nec, pulvinar porttitor lorem. Pellentesque dolor elit.</p>
+                    <div className='tl-content'>
+                        <h1>Activities I Enjoy</h1>
+                        <p>Since my job involves a lot of sitting, I like a variety of active hobbies! These include rock climbing, biking, basketball, and photography.<br /><br />Although I don't have a website page dedicated to showing off my pictures or other hobbies, feel free to contact me if you think I should add a page for this content!</p>
+                    </div>
+                </div>
+                <div className='tl-item'>
+                    <div className='tl-bg' style={{backgroundImage: `url(${homeBackground1})`}}></div>
+                    <div className='tl-year'>
+                        <p>School</p>
+                    </div>
+                    <div className='tl-content'>
+                        <h1>My Educational Background</h1>
+                        <p>After taking AP courses in high school, I went straight into the engineering program at the University of Alberta. I recently graduated with my Computer Engineering degree.<br /><br />In the future, I am interested in going back for my MBA or other master's level business degree after getting more industry experience. In addition, I am strongly considering getting a certificate in personal training one day!</p>
                     </div>
                 </div>
             </div>
