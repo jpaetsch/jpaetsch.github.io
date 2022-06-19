e4909ade0830009d4ad92b5b57f55b6601c7cc92
jpaetsch <j.paetsch@live.com>
2022-06-19 16:10:50 -0600

    Home Page flex content text edits

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -50,7 +50,7 @@ const Home = () => {
                         <p>Work</p>
                     </div>
                     <div className='tl-content'>
-                        <h1>My Skills & Experience</h1>
+                        <h1>My Experience</h1>
                         <p>Throughout my degree and outside of school, I've worked at a number of companies and gained exposure to a wide range of technical skills.<br /><br />From backend system frameworks to frontend design libraries, I have experience in many different programming languages and architectural design concepts.</p>
                         <br />
                         <Link to='/about' className='sub-button'>View Skills</Link>
@@ -65,7 +65,7 @@ const Home = () => {
                         <h1>My Showcases</h1>
                         <p>There's no better way to learn than to get hands-on. I have a number of projects I like to develop outside of work to help with my own learning.<br /><br />One of these projects includes making this website from scratch using React and SCSS! See my GitHub repository for additional projects and code samples.</p>
                         <br />
-                        <Link to='/projects' className='sub-button'>Portfolio Summary</Link>
+                        <Link to='/projects' className='sub-button'>View Portfolio</Link>
                     </div>
                 </div>
                 <div className='tl-item'>
@@ -74,8 +74,8 @@ const Home = () => {
                         <p>Hobbies</p>
                     </div>
                     <div className='tl-content'>
-                        <h1>Activities I Enjoy</h1>
-                        <p>Since my job involves a lot of sitting, I like a variety of active hobbies! These include rock climbing, biking, basketball, and photography.<br /><br />Although I don't have a website page dedicated to showing off my pictures or other hobbies, feel free to contact me if you think I should add a page for this content!</p>
+                        <h1>My Activities</h1>
+                        <p>Since my job involves a lot of sitting, I like a variety of active hobbies! These include rock climbing, biking, basketball, and photography.<br /><br />Although I don't have a website page dedicated to showing off my pictures or other hobbies, contact me if you think I should add a page for this content!</p>
                         <br />                    
                         <Link to='/contact' className='sub-button'>Get In Touch</Link>
                     </div>
@@ -86,7 +86,7 @@ const Home = () => {
                         <p>School</p>
                     </div>
                     <div className='tl-content'>
-                        <h1>My Educational Background</h1>
+                        <h1>My Education</h1>
                         <p>After taking AP courses in high school, I went straight into the engineering program at the University of Alberta. I recently graduated with my Computer Engineering degree.<br /><br />In the future, I am interested in going back for my MBA or other master's level business degree after getting more industry experience. In addition, I am strongly considering getting a certificate in personal training one day!</p>
                     </div>
                 </div>
