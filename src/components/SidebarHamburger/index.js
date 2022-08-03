ea229f94c4b0c76c830a0c5af36d640ed3cfbb0d
jpaetsch <j.paetsch@live.com>
2022-08-03 13:06:36 -0600

    Added final buttons to mobile friendly sidebar

--- src/components/SidebarHamburger/index.js
+++ src/components/SidebarHamburger/index.js
@@ -1,9 +1,10 @@
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faHome, faUser, faFolderTree } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome, faUser, faFolderTree, faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavLink } from 'react-router-dom';
 import ProfilePic from '../../assets/images/profile-pic.jpg';
 import Signature from '../../assets/images/name-signature.png';
+import Resume from '../../assets/files/Resume.pdf';
 import './index.scss';
 
 
@@ -33,6 +34,17 @@ const SidebarHamburger = () => {
                         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />&nbsp;&nbsp;&nbsp;&nbsp;CONTACT
                     </NavLink>
                 </nav>
+                <div className='footer-buttons'>
+                    <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/jacob-paetsch/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                    <a target='_blank' rel='noreferrer' href='https://github.com/jpaetsch'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                    <a target='_blank' rel='noreferrer' href={Resume} className='contact-button' download>
+                        <FontAwesomeIcon icon={faFileArrowDown} color='#4d4d4e' />
+                    </a>
+                </div>
             </div>
         </div>
     );
