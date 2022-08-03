ef9392c43041a6975ae38a1ea7b9dc2d40a0b716
jpaetsch <j.paetsch@live.com>
2022-08-03 13:52:14 -0600

    Updates

--- src/components/Contexts/ThemeContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const initialThemeState = {
-  theme: 'original',
-  setTheme: () => null
-};
-
-const ThemeContext = React.createContext(initialThemeState);
-export default ThemeContext;
