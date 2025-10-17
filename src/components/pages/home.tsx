6f21c7a23c1b8105d9d2fff266e017527ccf3b03
Jacob Paetsch <j.paetsch@live.com>
2025-10-17 14:44:52 -0600

    Adjusted home styling

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,6 +1,6 @@
 export const HomePage = () => {
   return (
-    <div className="bg-background text-foreground min-h-screen p-4">
+    <div className="p-4">
       <h1 className="text-2xl">Hello World</h1>
     </div>
   );
